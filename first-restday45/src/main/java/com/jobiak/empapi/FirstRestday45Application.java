9ffd846b080f2c6f3580c92a2c4e298b2b0aee88
CheerladinneMouli <105494702+CheerladinneMouli@users.noreply.github.com>
2022-05-17 10:52:24 +0530

    done

---
+++ first-restday45/src/main/java/com/jobiak/empapi/FirstRestday45Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.empapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestday45Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestday45Application.class, args);
+	}
+
+}
